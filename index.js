57f4bba0b431386f3ccd396d0a8bb551ead9e68e
unknown <ismaili.alaoui.ibrahim.99@gmail.com>
2025-02-01 19:57:26 +0100

    my project X&O

---
+++ index.js
@@ -0,0 +1,78 @@
+const cells = document.querySelectorAll(".cell");
+const statusText = document.querySelector("#statusText");
+const restartBtn= document.querySelector("#restartBtn");
+const winconditions =[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6] 
+];
+let options = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = "X";
+let running =false;
+
+initializeGame();
+
+function initializeGame(){
+    cells.forEach(cell=>cell.addEventListener("click",cellcliecked));
+    restartBtn.addEventListener("click", restertGame);
+    statusText.textContent= `${currentPlayer}'s turn`;
+    running = true;
+}
+function cellcliecked(){
+ const cellIndex = this.getAttribute("cellIndex");
+ if( options[cellIndex]!=""|| !running){
+    return;
+ }
+ updatecell(this,cellIndex);
+ checkwinner();
+}
+function updatecell(cell,index){
+  options[index]=currentPlayer;
+  cell.textContent=currentPlayer;
+}
+function chengePlayer(){
+currentPlayer =(currentPlayer=="X")? "O" :"X";
+statusText.textContent=`${currentPlayer}'s turn`;
+}
+function checkwinner(){
+let roundwon =false;
+
+for(let i=0;i<winconditions.length;i++ ){
+    const condition = winconditions[i];
+    const cellA =options[condition[0]];
+    const cellB =options[condition[1]];
+    const cellC =options[condition[2]];
+
+    if(cellA==""|| cellB==""||cellC==""){
+        continue;
+    }
+    if (cellA==cellB && cellB==cellC){
+        roundwon=true;
+        break;
+    }
+}
+if(roundwon){
+    statusText.textContent =`${currentPlayer} wins!`;
+    running =false;
+}
+else if(!options.includes("")){
+    statusText.textContent =`Draw!`;
+    running =false;
+}
+else{
+    chengePlayer();
+}
+}
+function restertGame(){
+    currentPlayer ="X";
+    options = ["", "", "", "", "", "", "", "", ""];
+    statusText.textContent= `${currentPlayer}'s turn`;
+    cells.forEach(cell=>cell.textContent ="");
+    running =true;
+
+}
