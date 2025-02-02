df5b416de5a33792d6928649a14dd7c4ecfca3ec
Brahim Ismaili Alaoui <111463395+ibra0011@users.noreply.github.com>
2025-02-02 14:34:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const cells = document.querySelectorAll(".cell");
 const statusText = document.querySelector("#statusText");
-const restartBtn= document.querySelector("#restartBtn");
+const restartBtn = document.querySelector("#restartBtn");
 const winConditions = [
     [0,1,2],
     [3,4,5],
@@ -18,7 +18,7 @@ let running = false;
 initializeGame();
 
 function initializeGame() {
-    cells.forEach(cell=> cell.addEventListener("click", cellClicked));
+    cells.forEach( cell=> cell.addEventListener("click", cellClicked));
     restartBtn.addEventListener("click", restertGame);
     statusText.textContent = `${currentPlayer}'s turn`;
     running = true;
@@ -29,7 +29,7 @@ function cellClicked() {
     if (options[cellIndex] !="" || !running) {
         return;
      }
-    updateCell(this, cellIndex);
+    updateCell( this , cellIndex );
     checkWinner();
 }
 
@@ -38,44 +38,45 @@ function updateCell(cell,index) {
     cell.textContent = currentPlayer;
 }
 
-function chengePlayer(){
-currentPlayer =(currentPlayer=="X")? "O" :"X";
-statusText.textContent=`${currentPlayer}'s turn`;
+function changePlayer() {
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+statusText.textContent = `${currentPlayer}'s turn`;
 }
-function checkwinner(){
-let roundwon =false;
 
-for(let i=0;i<winconditions.length;i++ ){
+function checkWinner() {
+let roundwon = false;
+
+for(let i = 0 ; i < winconditions.length ; i++ ) {
     const condition = winconditions[i];
-    const cellA =options[condition[0]];
-    const cellB =options[condition[1]];
-    const cellC =options[condition[2]];
+    const cellA = options[condition[0]];
+    const cellB = options[condition[1]];
+    const cellC = options[condition[2]];
 
-    if(cellA==""|| cellB==""||cellC==""){
+    if(cellA == "" || cellB == "" || cellC == "" ) {
         continue;
     }
-    if (cellA==cellB && cellB==cellC){
-        roundwon=true;
+    if (cellA == cellB && cellB == cellC ) {
+        roundwon = true;
         break;
     }
 }
-if(roundwon){
-    statusText.textContent =`${currentPlayer} wins!`;
-    running =false;
+if(roundwon) {
+    statusText.textContent = `${currentPlayer} wins!`;
+    running = false;
 }
-else if(!options.includes("")){
-    statusText.textContent =`Draw!`;
-    running =false;
+else if(!options.includes("")) {
+    statusText.textContent = `Draw!`;
+    running = false;
 }
-else{
+else {
     chengePlayer();
 }
 }
-function restertGame(){
-    currentPlayer ="X";
+function restertGame() {
+    currentPlayer = "X";
     options = ["", "", "", "", "", "", "", "", ""];
-    statusText.textContent= `${currentPlayer}'s turn`;
-    cells.forEach(cell=>cell.textContent ="");
-    running =true;
+    statusText.textContent = `${currentPlayer}'s turn`;
+    cells.forEach(cell=>cell.textContent = "");
+    running = true;
 
 }
