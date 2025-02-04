4bd3604cefa8d6e4d787c691a81aa4779b4d9466
Brahim Ismaili Alaoui <ismaili.alaoui.ibrahim.99@gmail.com>
2025-02-04 15:11:30 +0100

    new

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 window.onload =()=> {
+    const select = document.querySelector("#size");
     const cells = document.querySelectorAll(".cell");
     const statusText = document.querySelector("#statusText");
     const restartBtn = document.querySelector("#restartBtn");
-    const winConditions = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ];
+    const tab = Array ;
+    
     let options = ["", "", "", "", "", "", "", "", ""];
     let currentPlayer = "X";
     let running = false;
+
+    function tablau(n){
+        for(i==0; i<=n; i++){
+            for(j==0; j<=n; j++){
+                tab =[i][j];
+                const cellcontainer = document.querySelector("")
+            }
+        }
+    }
 
     initializeGame();
 
